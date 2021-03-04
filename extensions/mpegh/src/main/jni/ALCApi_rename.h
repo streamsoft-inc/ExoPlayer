14279975d3c81027a20e8f1f0a43c57ac03a6c90
Andrija Milovanovic <ender357@gmail.com>
2021-03-04 19:01:57 +0100

    Update c code and libraries

--- extensions/mpegh/src/main/jni/ALCApi_rename.h
+++ extensions/mpegh/src/main/jni/ALCApi_rename.h
@@ -10,8 +10,8 @@
 
 #pragma once
 
-#define alc_init 				t75u0149qbmywfaze2kdhpvoxn
-#define alc_set					djbhsx9ci4a5g1e2wzlrkq7o0u
-#define alc_proc				o1d4rv0ik6yqzljsben7w89xcm
-#define alc_get_version			yv68m34scoazq90p5wbdlg1i7k
-#define alc_get_worksize		hlyb21ox8395zfwdr7tme0kn6j
+#define alc_init            t75u0149qbmywfaze2kdhpvoxn
+#define alc_set             djbhsx9ci4a5g1e2wzlrkq7o0u
+#define alc_proc            o1d4rv0ik6yqzljsben7w89xcm
+#define alc_get_version     yv68m34scoazq90p5wbdlg1i7k
+#define alc_get_worksize    hlyb21ox8395zfwdr7tme0kn6j
--- extensions/mpegh/src/main/jni/libSonyIA_mobile.h
+++ extensions/mpegh/src/main/jni/libSonyIA_mobile.h
@@ -6,9 +6,8 @@
 #define __LIB_SIA_MOBILE_H__
 
 #ifdef __cplusplus
-//#define EXTERNC extern "C"
 #if defined(__WIN32__)
-#define EXTERNC extern "C" __declspec(dllexport) 
+#define EXTERNC extern "C" __declspec(dllexport)
 #elif defined(__GNUC__) && __GNUC__ >= 4
 #define EXTERNC extern "C" __attribute__ ((visibility("default")))
 #elif defined(__GNUC__) && __GNUC__ >= 2
@@ -28,42 +27,41 @@
 #define SIA_ERR_TYPE_2           5
 #define SIA_ERR_TYPE_3           6
 #define SIA_ERR_TYPE_4           7
+#define SIA_ERR_INVALID_API_CALL 8
 
-typedef struct _sia_mha_struct_ *SIA_MHA_HANDLE;
+typedef struct _sia_mhdr_struct_ *SIA_MHDR_HANDLE;
 
-typedef struct _sia_mha_param_ {
-  int reserved[16];
-} SIA_MHA_PARAM;
+typedef struct _sia_mhdr_param_ {
+		int reserved[16];
+} SIA_MHDR_PARAM;
 
 
-EXTERNC unsigned int sia_mha_getVersion(void);
-EXTERNC int sia_mha_getHandle(SIA_MHA_HANDLE *phSiaDec, const char *appRootPath, const char *fnameCoef1, const char *fnameCoef2);
-EXTERNC void sia_mha_freeHandle(SIA_MHA_HANDLE hSiaDec);
-EXTERNC int sia_mha_init(SIA_MHA_HANDLE hSiaDec, SIA_MHA_PARAM *pParam);
-EXTERNC int sia_mha_close(SIA_MHA_HANDLE hSiaDec);
-EXTERNC int sia_mha_reset(SIA_MHA_HANDLE hSiaDec);
+EXTERNC unsigned int sia_mhdr_getVersion(void);
+EXTERNC int sia_mhdr_getHandle(SIA_MHDR_HANDLE *phSiaDec);
+EXTERNC int sia_mhdr_freeHandle(SIA_MHDR_HANDLE *phSiaDec);
+EXTERNC int sia_mhdr_init(SIA_MHDR_HANDLE hSiaDec, SIA_MHDR_PARAM *pParam);
+EXTERNC int sia_mhdr_close(SIA_MHDR_HANDLE hSiaDec);
+EXTERNC int sia_mhdr_reset(SIA_MHDR_HANDLE hSiaDec);
 
-EXTERNC int sia_mha_procFrame(SIA_MHA_HANDLE hSiaDec, int *isLastFrame, float **pOutput);
+EXTERNC int sia_mhdr_procFrame(SIA_MHDR_HANDLE hSiaDec, int *isLastFrame, float **pOutput, int *pFlagPost);
 
-EXTERNC int sia_mha_fOpen(SIA_MHA_HANDLE hSiaDec, char *inFile);
-EXTERNC int sia_mha_fReadFrame(SIA_MHA_HANDLE hSiaDec, int *isLastFrame);
-EXTERNC int sia_mha_fClose(SIA_MHA_HANDLE hSiaDec);
+EXTERNC int sia_mhdr_fOpen(SIA_MHDR_HANDLE hSiaDec, char *inFile);
+EXTERNC int sia_mhdr_fReadFrame(SIA_MHDR_HANDLE hSiaDec, int *isLastFrame);
+EXTERNC int sia_mhdr_fClose(SIA_MHDR_HANDLE hSiaDec);
 
-EXTERNC int sia_mha_bsOpen(SIA_MHA_HANDLE hSiaDec, unsigned char *bs_in, long bs_size);
-EXTERNC int sia_mha_bsReadFrame(SIA_MHA_HANDLE hSiaDec, unsigned char *bs_in, long bs_size, int *isLastFrame);
-EXTERNC int sia_mha_bsClose(SIA_MHA_HANDLE hSiaDec);
+EXTERNC int sia_mhdr_bsOpen(SIA_MHDR_HANDLE hSiaDec, unsigned char *bs_in, long bs_size);
+EXTERNC int sia_mhdr_bsReadFrame(SIA_MHDR_HANDLE hSiaDec, unsigned char *bs_in, long bs_size, int *isLastFrame);
+EXTERNC int sia_mhdr_bsClose(SIA_MHDR_HANDLE hSiaDec);
 
-EXTERNC void sia_mha_getErrorDetail(SIA_MHA_HANDLE hSiaDec, int *pErrorCode, int *pErrorDetail);
-EXTERNC unsigned int sia_mha_getNumObjects(SIA_MHA_HANDLE hSiaDec);
-EXTERNC unsigned int sia_mha_getTotalNumFrames(SIA_MHA_HANDLE hSiaDec);
-EXTERNC unsigned int sia_mha_getCurrentFrameIndex(SIA_MHA_HANDLE hSiaDec);
-EXTERNC int sia_mha_setFrameIndex(SIA_MHA_HANDLE hSiaDec, unsigned int index);
+EXTERNC void sia_mhdr_getErrorDetail(SIA_MHDR_HANDLE hSiaDec, int *pErrorCode, int *pErrorDetail);
+EXTERNC unsigned int sia_mhdr_getNumObjects(SIA_MHDR_HANDLE hSiaDec);
+EXTERNC unsigned int sia_mhdr_getTotalNumFrames(SIA_MHDR_HANDLE hSiaDec);
+EXTERNC unsigned int sia_mhdr_getCurrentFrameIndex(SIA_MHDR_HANDLE hSiaDec);
+EXTERNC int sia_mhdr_setFrameIndex(SIA_MHDR_HANDLE hSiaDec, unsigned int index);
 
-EXTERNC int sia_mha_rawbsOpen(SIA_MHA_HANDLE hSiaDec, unsigned char *bs_in, long bs_size);
-EXTERNC int sia_mha_rawbsReadFrame(SIA_MHA_HANDLE hSiaDec, unsigned char *bs_in, long bs_size, int *isLastFrame);
-EXTERNC int sia_mha_rawbsClose(SIA_MHA_HANDLE hSiaDec);
+EXTERNC int sia_mhdr_rawbsOpen(SIA_MHDR_HANDLE hSiaDec, unsigned char *bs_in, long bs_size);
+EXTERNC int sia_mhdr_rawbsReadFrame(SIA_MHDR_HANDLE hSiaDec, unsigned char *bs_in, long bs_size, int *isLastFrame);
+EXTERNC int sia_mhdr_rawbsClose(SIA_MHDR_HANDLE hSiaDec);
 
-EXTERNC int sia_coef1_getVersion(SIA_MHA_HANDLE hSiaDec, signed char *name, unsigned char *sum, 
-								int *year, int *month, int *day, int *hour, int *min);
-EXTERNC int sia_coef2_getVersion(SIA_MHA_HANDLE hSiaDec, signed char *name, unsigned char *sum);
+EXTERNC unsigned int sia_mhdr_getRelVersion(void);
 #endif /* __LIB_SIA_MOBILE_H__ */
